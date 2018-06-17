af0833c1a4b546b4632032d775d55334951bc7f1
morozzzz <morozzz.ilya@gmail.com>
2018-06-17 15:18:56 +0300

    feature/ play button added, author info added

--- paper-js-stars.js
+++ paper-js-stars.js
@@ -87,9 +87,9 @@ function onMouseMove(event) {
         return -1 + 2*val;
     }
     vector.x = polarize(d.x < 0);
-    rotation.x = polarize(d.x < 0)*0;
+    rotation.x = polarize(d.x < 0);
     vector.y = polarize(d.y < 0);
-    rotation.y = polarize(d.y < 0)*0;
+    rotation.y = polarize(d.y < 0);
 
     var da = new Point(Math.abs(d.x), Math.abs(d.y));
     da *= mouseScale/5;
